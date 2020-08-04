06937c050feb5f5b21ad25d1fb79fcad5b6a510c
Benjamin Kataliko Viranga <31964614+benjivrik@users.noreply.github.com>
2020-08-04 17:52:02 -0400

    Update Q2.java

--- Homework2/Q2.java
+++ Homework2/Q2.java
@@ -1,11 +1,10 @@
 /**
  * 
  * Author : Benjamn Kataliko Viranga
- * ID : 8842942
  * 
- * Class : CSI3531 - Summer 2020 - Devoir 2
  * 
- * University of Ottawa 
+ * Class : CSI3531 - Devoir 2
+ * 
  * 
  * How to run from the command line ?
  * 
